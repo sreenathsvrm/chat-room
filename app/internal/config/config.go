7cf8d2c98bfe9ba86e84984abca6f84853b3f1ee
sreenathmantech <sreenath@mantechventures.com>
2025-07-18 21:37:13 +0530

    config updates with loading function

--- app/internal/config/config.go
+++ app/internal/config/config.go
@@ -1,1 +1,43 @@
 package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     int
+	DBUser     string
+	DBPassword string
+	DBName     string
+}
+
+func LoadConfig() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	dbPort, err := strconv.Atoi(getEnv("DB_PORT", "5432"))
+	if err != nil {
+		log.Fatal("Invalid DB_PORT")
+	}
+
+	return &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     dbPort,
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
+		DBName:     getEnv("DB_NAME", "chat"),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module chatapp
 
 go 1.24.2
+
+require github.com/joho/godotenv v1.5.1 // indirect
